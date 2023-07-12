e3c4f095898febf361449db206d62f08a6d09103
Connor De Meyer <connordemeyer@gmail.com>
2023-07-12 16:26:31 +0900

    Revert "Revert "optional parameter names for glass auto for typeTuple""
    This reverts commit 1d0761b82224af89671be47c407dc872eafb8002.

--- examples/ImGui/ImGuiEditor/ImGuiWrappers.h
+++ examples/ImGui/ImGuiEditor/ImGuiWrappers.h
@@ -11,12 +11,18 @@ struct ImGuiWrapper_InputText final
 	std::string Label{};
 	std::string Hint{};
 	std::string Content{};
+
+	void Draw()
+	{
+		ImGui::InputText(Label.c_str(), &Content);
+	}
 };
 
 GLAS_TYPE(ImGuiWrapper_InputText)
 GLAS_MEMBER(ImGuiWrapper_InputText, Label)
 GLAS_MEMBER(ImGuiWrapper_InputText, Hint)
 GLAS_MEMBER(ImGuiWrapper_InputText, Content)
+GLAS_MEMBER_FUNCTION(ImGuiWrapper_InputText, Draw);
 
 struct ImGuiWrapper_Button final
 {
