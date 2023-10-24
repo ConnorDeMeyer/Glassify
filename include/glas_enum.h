840c193347149ec006e9f3ba364cbe4ea405af26
Connor De Meyer <connordemeyer@gmail.com>
2023-10-24 12:06:10 +0900

    Comments

--- include/glas_enum.h
+++ include/glas_enum.h
@@ -12,6 +12,10 @@ inline constexpr bool EnumEnableBitOperations = false;
 template <typename Enum>
 concept BitFieldOperators = EnumEnableBitOperations<Enum> && std::is_enum_v<Enum>;
 
+/**
+ * Enables the bit OR operation to be used on enums. Example:
+ * Property::prop0 | Property::prop1
+ */
 template <typename Enum>
 constexpr Enum operator|(Enum lhs, Enum rhs) requires BitFieldOperators<Enum>
 {
@@ -19,6 +23,9 @@ constexpr Enum operator|(Enum lhs, Enum rhs) requires BitFieldOperators<Enum>
 	return static_cast<Enum>(static_cast<T>(lhs) | static_cast<T>(rhs));
 }
 
+/**
+ * Enables the bit AND operator to be used with enums.
+ */
 template <typename Enum>
 constexpr Enum operator&(Enum lhs, Enum rhs) requires BitFieldOperators<Enum>
 {
@@ -26,6 +33,9 @@ constexpr Enum operator&(Enum lhs, Enum rhs) requires BitFieldOperators<Enum>
 	return static_cast<Enum>(static_cast<T>(lhs) & static_cast<T>(rhs));
 }
 
+/**
+ * Enables the bit XOR operator the be used with enums.
+ */
 template <typename Enum>
 constexpr Enum operator^(Enum lhs, Enum rhs) requires BitFieldOperators<Enum>
 {
@@ -33,6 +43,9 @@ constexpr Enum operator^(Enum lhs, Enum rhs) requires BitFieldOperators<Enum>
 	return static_cast<Enum>(static_cast<T>(lhs) ^ static_cast<T>(rhs));
 }
 
+/**
+ * Enables the bit NOT operator to be used with enums.
+ */
 template <typename Enum>
 constexpr Enum operator~(Enum val) requires BitFieldOperators<Enum>
 {
@@ -40,6 +53,9 @@ constexpr Enum operator~(Enum val) requires BitFieldOperators<Enum>
 	return static_cast<Enum>(~static_cast<T>(val));
 }
 
+/**
+ * Enables the bool NOT operator to be used with enums.
+ */
 template <typename Enum>
 constexpr bool operator!(Enum val) requires BitFieldOperators<Enum>
 {
